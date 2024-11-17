0380eebc28cf6edea4aea45e8a7844ed4cf4211a
surendra <tamangsurendra44@gmail.com>
2024-11-17 16:11:24 +0000

    Added the link

--- web-scraper/src/main.rs
+++ web-scraper/src/main.rs
@@ -7,9 +7,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Create an HTTP client
     let client = reqwest::Client::new();
 
+    let url = "https://jyaba.tech/";
+
     // Make a GET request to a website
     let response = client
-        .get("https://getdatafor.me/")
+        .get(url)
         .header("User-Agent", "Mozilla/5.0")
         .send()
         .await?;
@@ -23,6 +25,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Create a selector for h1 elements
     let h1_selector = Selector::parse("h1").unwrap();
     let p_selector = Selector::parse("p").unwrap();
+    let link_selector = Selector::parse("a").unwrap();
 
     // Find and print all h1 elements
     println!("Finding h1 elements:");
@@ -34,6 +37,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("\nFinding paragraph elements:");
     for element in document.select(&p_selector) {
         println!("- {}", element.text().collect::<Vec<_>>().join(" "));
+    }
+
+    // Find and print all links
+    println!("\nFinding links:");
+    for element in document.select(&link_selector) {
+        if let Some(href) = element.value().attr("href") {
+            let text = element.text().collect::<Vec<_>>().join(" ");
+            if !text.is_empty() {
+                println!("Text: {}", text);
+                println!("URL: {}", href);
+                println!("---");
+            }
+        }
     }
 
     Ok(())
